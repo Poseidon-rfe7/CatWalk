22fe94e8bbe5dc247f4beb3fb42bbe9f5efb6a76
Cory Zauss <coryclassic1989@gmail.com>
2021-12-23 20:42:46 -0500

    api work

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
+const path = require('path')
 
-// app.use(express.static('public'))
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
 app.use(express.json())
 app.use(express.static("client/public"));
 
 
-app.get('/', (req, res) => {
+app.get('/api/products', (req, res) => {
+    // console.log(req)
+  let url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products';
+  let header = {
+    'User-Agent': 'request',
+    'Authorization': process.env.GITHUB_API_KEY
+  }
+
+  axios.get(url, header)
+  .then(result => console.log(result))
+  .catch(err => res.send(err));
 
 })
 
