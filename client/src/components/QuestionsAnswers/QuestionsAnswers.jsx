3619aef7ebb8e637a52a4a7afbc5319dd91682a9
Jared Maldonado <jrmaldo1228@gmail.com>
2021-12-31 03:40:58 -0600

    Setup basic layout for Q&A module

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import Search from './Search.jsx';
+import QuestionsList from './QuestionsList.jsx';
 
-const QuestionsAnswers = () => (
-  <div>
-    QuestionsAnswers
-  </div>
+class QuestionsAnswers extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showHideMoreQuestions: true
+    };
+  }
 
-)
+  componentDidMount() {
+    // console.log(this.props)
+    axios.get(`/api/qa/questions/?product_id=37317`)
+    .then((result) => {console.log(result.data)});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Questions & Answers</h2>
+        <Search />
+        <QuestionsList />
+        {this.state.showHideMoreQuestions && <button>More Answered Questions</button>}
+        <button>Add a Question +</button>
+      </div>
+    )
+  }
+}
 
 export default QuestionsAnswers
