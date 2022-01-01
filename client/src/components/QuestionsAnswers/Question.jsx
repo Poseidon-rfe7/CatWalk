16b75e06c7c8f73ab14b487fe0dd340ebe930568
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-01 17:01:48 -0600

    Renders a single question

--- client/src/components/QuestionsAnswers/Question.jsx
+++ client/src/components/QuestionsAnswers/Question.jsx
@@ -4,9 +4,16 @@ import AnswersList from './AnswersList.jsx'
 const Question = (props) => {
   return (
     <div>
-      <span>Q: This is a question?</span>
-      <span>Helpful? <button>Yes (25)</button> | <button>Add Answer</button></span>
-      <AnswersList />
+      <div className='single-question'>
+        Q: {props.question.question_body}
+        <span className='qa-helpful q-helpful'>
+          Helpful?
+          <button className='qa-button helpful-button'>Yes ({props.question.question_helpfulness})</button>
+          |
+          <button className='qa-button helpful-button'> Add Answer</button>
+        </span>
+      </div>
+      {props.question.answers.length > 0 && <AnswersList answers={props.question.answers} />}
     </div>
   )
 };
