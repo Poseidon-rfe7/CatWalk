a3aec19076e1cb176e6dc63b06bb64f13a78f1ab
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-01 17:03:09 -0600

    Added search bar and submit button

--- client/src/components/QuestionsAnswers/Search.jsx
+++ client/src/components/QuestionsAnswers/Search.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Search = (props) => {
   return (
-    <div>
-      <input type='text' placeholder='Have a question? Search for answers...'></input>
-      <button>Search</button>
+    <div className='search-questions'>
+      <input type='text' placeholder='Have a question? Search for answers...' className='search-bar'></input>
+      <button className='qa-button submit-search'>Search</button>
     </div>
   )
 };
