c49d97830be46849e8da85d57bac2e2ff7abd934
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-01 17:02:29 -0600

    Renders entire questions list and appends them to DOM

--- client/src/components/QuestionsAnswers/QuestionsList.jsx
+++ client/src/components/QuestionsAnswers/QuestionsList.jsx
@@ -4,7 +4,8 @@ import Question from './Question.jsx';
 const QuestionsList = (props) => {
   return (
     <div>
-      <Question />
+      {props.questions.map((question) =>
+      <Question key={question.question_id} question={question} />)}
     </div>
   )
 };
