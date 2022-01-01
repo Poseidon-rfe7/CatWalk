4b4b359af86477e3541e14bf44fb8d35a6abd862
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-01 17:00:04 -0600

    Renders a single answer

--- client/src/components/QuestionsAnswers/Answer.jsx
+++ client/src/components/QuestionsAnswers/Answer.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const Answer = (props) => {
   return (
-    <div>
-      <div>A: This is an answer.</div>
-      <span>by User1234, December 29, 2021 | Helpful? <button>Yes (2)</button> | <button>Report</button></span>
+    <div className='answer'>
+      <p className='reset-margins'>{props.answer.body}</p>
+      {props.answer.photos.length && props.answer.photos.map((photo, i) => {
+        return <img src={photo} key={i} className='photos-thumbnail' />
+      })}
+      <div className='qa-helpful a-helpful'>
+        by {props.answer.answerer_name}, {props.answer.date} | Helpful?
+        <button className='qa-button helpful-button'>Yes ({props.answer.helpfulness})</button>
+        |
+        <button className='qa-button helpful-button'>Report</button>
+      </div>
     </div>
   )
 };
