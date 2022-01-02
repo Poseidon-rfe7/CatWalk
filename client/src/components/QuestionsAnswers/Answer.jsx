8291d7314a0bd8372a0eb728f312df79a8a14a27
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-02 01:53:00 -0600

    Q&A now renders with real data

--- client/src/components/QuestionsAnswers/Answer.jsx
+++ client/src/components/QuestionsAnswers/Answer.jsx
@@ -4,8 +4,8 @@ const Answer = (props) => {
   return (
     <div className='answer'>
       <p className='reset-margins'>{props.answer.body}</p>
-      {props.answer.photos.length && props.answer.photos.map((photo, i) => {
-        return <img src={photo} key={i} className='photos-thumbnail' />
+      {props.answer.photos.length && props.answer.photos.map((photo) => {
+        return <img src={photo.url} key={photo.id} className='photos-thumbnail' />
       })}
       <div className='qa-helpful a-helpful'>
         by {props.answer.answerer_name}, {props.answer.date} | Helpful?
--- client/src/components/QuestionsAnswers/Question.jsx
+++ client/src/components/QuestionsAnswers/Question.jsx
@@ -1,21 +1,70 @@
 import React from 'react';
-import AnswersList from './AnswersList.jsx'
+import AnswersList from './AnswersList.jsx';
+import axios from 'axios';
 
-const Question = (props) => {
-  return (
-    <div>
-      <div className='single-question'>
-        Q: {props.question.question_body}
-        <span className='qa-helpful q-helpful'>
-          Helpful?
-          <button className='qa-button helpful-button'>Yes ({props.question.question_helpfulness})</button>
-          |
-          <button className='qa-button helpful-button'> Add Answer</button>
-        </span>
+// const Question = (props) => {
+//   console.log(props.getAnswers(props.question.question_id));
+
+//   return (
+//     <div>
+//       <div className='single-question'>
+//         Q: {props.question.question_body}
+//         <span className='qa-helpful q-helpful'>
+//           Helpful?
+//           <button className='qa-button helpful-button'>Yes ({props.question.question_helpfulness})</button>
+//           |
+//           <button className='qa-button helpful-button'> Add Answer</button>
+//         </span>
+//       </div>
+//       {/* {answers.length > 0 && <AnswersList answers={getAnswersList} />} */}
+//     </div>
+//   )
+// };
+
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: []
+    }
+  }
+
+  componentDidMount() {
+    this.getAnswers(this.props.question.question_id);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.question.question_id !== this.props.question.question_id) {
+    this.getAnswers(this.props.question.question_id);
+    }
+  }
+
+  getAnswers(question_id) {
+    axios.get(`/api/qa/questions/${question_id}/answers`)
+      .then((response) => {
+        this.setState({ answers: response.data.results });
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className='single-question'>
+          Q: {this.props.question.question_body}
+          <span className='qa-helpful q-helpful'>
+            Helpful?
+            <button className='qa-button helpful-button'>Yes ({this.props.question.question_helpfulness})</button>
+            |
+            <button className='qa-button helpful-button'> Add Answer</button>
+          </span>
+        </div>
+        {this.state.answers.length > 0 && <AnswersList answers={this.state.answers} />}
       </div>
-      {props.question.answers.length > 0 && <AnswersList answers={props.question.answers} />}
-    </div>
-  )
-};
+    )
+  }
+}
 
 export default Question;
