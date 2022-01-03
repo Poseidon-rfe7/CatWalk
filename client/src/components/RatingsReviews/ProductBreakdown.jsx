9eedbc69f7841a834732abcb6056eb03e01b72a8
“Allen <allenalejandro3@gmail.com>
2022-01-03 02:22:04 -0500

    added product and rating breakdown

---
+++ client/src/components/RatingsReviews/ProductBreakdown.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ProductBreakdownProgressBar from './ProductBreakdownProgressBar.jsx';
+
+class ProductBreakdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'test'
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        {console.log(this.props.meta)}
+      <ProductBreakdownProgressBar characteristics={this.props.meta.characteristics} characteristic={'Comfort'}/>
+
+      </div>
+    )
+  }
+}
+
+export default ProductBreakdown;
