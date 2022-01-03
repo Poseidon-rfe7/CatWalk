a85a1ee1a3653634220b339e792791767ded2109
“Allen <allenalejandro3@gmail.com>
2022-01-03 10:12:07 -0500

    removed console.log

--- client/src/components/RatingsReviews/ProductBreakdown.jsx
+++ client/src/components/RatingsReviews/ProductBreakdown.jsx
@@ -14,6 +14,7 @@ class ProductBreakdown extends React.Component {
     return (
       <div>
         {/* {console.log(this.props.meta)} */}
+
       <ProductBreakdownProgressBar characteristics={this.props.meta.characteristics} characteristic={'Comfort'}/>
 
       </div>
