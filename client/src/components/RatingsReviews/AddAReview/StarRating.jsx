78fcc402418f3aae88042a04d30071e22ef27553
“Allen <allenalejandro3@gmail.com>
2022-01-04 16:57:01 -0500

    added star rating component and css

---
+++ client/src/components/RatingsReviews/AddAReview/StarRating.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function StarRating(props) {
+  return (
+    <div className='star-rating-container'>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
+        return (
+          <label>
+            <input
+              className='star-rating-radio'
+              type='radio'
+              name='rating'
+              value={ratingValue}
+              onClick={() => props.starClickHandler(ratingValue)}
+            />
+            <div>
+              <span
+              className='star-rating-icon'
+                style={{color: ratingValue <= (props.hover || props.rating) ? '#ffc107' : '#ccc'}}
+                onMouseEnter={() => props.starMouseEnter(ratingValue)}
+                onMouseLeave={props.starMouseLeave}
+              ><i className="fas fa-star"></i></span>
+            </div>
+          </label>
+        )
+      })}
+    </div>
+  )
+}
+
+export default StarRating;
+
+
