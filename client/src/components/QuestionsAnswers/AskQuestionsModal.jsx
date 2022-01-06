09acf7f18a55df87876a6ced1eed0f7d8025a03a
Jared Maldonado <jrmaldo1228@gmail.com>
2022-01-06 13:37:09 -0600

    Basic layout for ask a question modal

--- client/src/components/QuestionsAnswers/AskQuestionsModal.jsx
+++ client/src/components/QuestionsAnswers/AskQuestionsModal.jsx
@@ -7,6 +7,12 @@ const AskQuestionsModal = (props) => {
         <h1>Ask Your Question</h1>
         <h3>About the {props.currentProduct.name}</h3>
         <form>
+          <label htmlFor='email-input'>Email: </label>
+          <input id='email-input' type='email' placeholder='Example: jack@email.com' />
+          <label htmlFor='nickname-input'>Nickname: </label>
+          <input id='nickname-input' type='text' placeholder='Example: jackson11!' /><br />
+          <label htmlFor='question-input'>Your question:</label><br />
+          <textarea id='question-input' maxLength='1000' placeholder='Ask your question...' /><br />
           <input type='submit' value='Submit' />
           <input type='button' value='Cancel' />
         </form>
