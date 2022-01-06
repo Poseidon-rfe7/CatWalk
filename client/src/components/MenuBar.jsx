dff543e42833848c45fb4432f5f948a5078cf7df
Cory Zauss <coryclassic1989@gmail.com>
2022-01-06 11:41:55 -0500

    menu layout

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -4,7 +4,26 @@ const MenuBar = () => {
 
   return (
     <div className="menu-bar">
-       <h2 id="main-title">CatWalk</h2>
+      <p className="whitespace"></p>
+       <h2 id="main-title"> CatWalk <i class="fas fa-cat"></i></h2>
+       <p className="whitespace"></p>
+       <a className="menu-link">product overview</a>
+       <p className="whitespace"></p>
+       <hr className="menu-link"/>
+       <p className="whitespace"></p>
+       <a className="menu-link">related products</a>
+       <p className="whitespace"></p>
+       <hr className="menu-link"/>
+       <p className="whitespace"></p>
+       <a className="menu-link">questions&answers</a>
+       <p className="whitespace"></p>
+       <hr className="menu-link"/>
+       <p className="whitespace"></p>
+       <a className="menu-link">reviews</a>
+       <p className="whitespace"></p>
+
+
+
     </div>
   )
 }
